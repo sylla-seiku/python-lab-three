7de25b24198f4c0f072ec13106fb32d8f8e8265a
sylla-seiku <seikusylla@gmail.com>
2023-09-08 15:23:30 -0400

    Learning For Loops.

--- python.py
+++ python.py
@@ -1,9 +1,34 @@
+# Learning For Loops
+for item in ['Seiku', 'Beyage', 'Ebrahim']: # "[]" this right here defines a list.
+    print(item)
+
+for item in [1, 2, 3, 4,]:
+    print(item)
+
+for item in range(5, 10, 2): #range creats an object we can irrerate over. the first num is count 2nd num it ends 3rd num is the steps
+    print(item)
 
 
 
+for item in 'Python':
+    print(item)
+
+#practice
+prices = [10, 20, 30]
+
+total = 0
+for price in prices:
+    total += price
+print(f"Total: {total}")
+
+
+
+
+'''
 #learning While Loops
 i = 1
 while i <= 5:
     print('*' * i)
     i += 1
 print("Done")
+'''
