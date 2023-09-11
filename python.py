f05faaa9edeee8d7816d950dc9d62863aaf794ab
sylla-seiku <seikusylla@gmail.com>
2023-09-11 11:06:29 -0400

    learning about nestead loops.

--- python.py
+++ python.py
@@ -1,5 +1,25 @@
 # Learning Nested Loops
-(x, y)
+for x in range(4):
+    print(x)
+
+for x in range(4):   #outer loop
+    for y in range(3):  #inner loop
+        print(f'({x}, {y})')
+
+#practice writting the letter "F" and "L" using nested loops
+numbers = [5, 2, 5, 2, 2]
+for x_count in numbers:
+    print('x' * x_count)
+
+
+numbers = [2, 2, 2, 2, 5]
+for x_count in numbers:
+    output = ''
+    for count in range(x_count):
+        output += 'x'
+
+    print(output)
+
 
 
 
